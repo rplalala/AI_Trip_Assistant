cefc959cb040b591cc21083beda04c667d3c558a
caorong001 <caorong001@ke.com>
2025-10-16 16:47:20 +1100

    Feat: Login page

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,7 +1,20 @@
-// src/App.tsx
-import './App.css'
-import AiConnectTest from './AiConnectTest'
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import MainLayout from './components/MainLayout';
+import HomePage from "./pages/HomePage";
+import PreferencePage from "./pages/PreferencePage";
+import LoginPage from "./pages/LoginPage";
+
 
 export default function App() {
-    return <AiConnectTest />
+    return (
+        <BrowserRouter>
+            <MainLayout>
+                <Routes>
+                    <Route path="/" element={<HomePage />} />
+                    <Route path="/preference" element={<PreferencePage />} />
+                    <Route path="/login" element={<LoginPage />} />
+                </Routes>
+            </MainLayout>
+        </BrowserRouter>
+    )
 }
---
+++ web/src/pages/LoginPage/index.tsx
@@ -0,0 +1,51 @@
+import type { FormProps } from 'antd';
+import { Button, Form, Input } from 'antd';
+
+type FieldType = {
+    email?: string;
+    password?: string;
+};
+
+const onFinish: FormProps<FieldType>['onFinish'] = (values) => {
+    console.log('Success:', values);
+};
+
+const onFinishFailed: FormProps<FieldType>['onFinishFailed'] = (errorInfo) => {
+    console.log('Failed:', errorInfo);
+};
+
+export default function LoginPage() {
+    return (
+        <Form
+            name="basic"
+            labelCol={{ span: 8 }}
+            wrapperCol={{ span: 16 }}
+            style={{ maxWidth: 600 }}
+            onFinish={onFinish}
+            onFinishFailed={onFinishFailed}
+            autoComplete="off"
+        >
+            <Form.Item<FieldType>
+                label="Email"
+                name="email"
+                rules={[{ type: 'email', required: true, message: 'Please input your email!' }]}
+            >
+                <Input />
+            </Form.Item>
+
+            <Form.Item<FieldType>
+                label="Password"
+                name="password"
+                rules={[{ required: true, message: 'Please input your password!' }]}
+            >
+                <Input.Password />
+            </Form.Item>
+
+            <Form.Item label={null}>
+                <Button type="primary" htmlType="submit">
+                    Log In Now
+                </Button>
+            </Form.Item>
+        </Form>
+    )
+}
