fd24a6d80268cae6a3298d262fe00496674c4406
Rong Cao <rcao0870@uni.sydney.edu.au>
2025-10-19 01:09:03 +1100

    Dev rong (#7)
    * Feat: Login page
    
    * Feat: Login & Register for frontend
    
    ---------
    
    Co-authored-by: caorong001 <caorong001@ke.com>

--- web/src/components/MainLayout.tsx
+++ web/src/components/MainLayout.tsx
@@ -1,5 +1,7 @@
-import { Layout, Menu, theme } from 'antd';
+import { Layout, Menu, theme, Avatar, Dropdown, Space } from 'antd';
 import { useNavigate, useLocation } from 'react-router-dom';
+import { UserOutlined, LogoutOutlined } from '@ant-design/icons';
+import { useAuth } from '../contexts/AuthContext'
 
 const { Header, Content, Footer } = Layout;
 
@@ -11,15 +13,47 @@ const items = [
 const MainLayout = ({ children }: any) => {
     const navigate = useNavigate();
     const location = useLocation();
+    const { user, setStatus, setUser } = useAuth();
 
     const {
         token: { colorBgContainer, borderRadiusLG },
     } = theme.useToken();
 
+    const handleLogout = () => {
+        localStorage.removeItem('token');
+        setUser(null);
+        setStatus('unauthenticated');
+        navigate('/');
+    };
+
+    const userMenu = {
+        items: [
+            { key: 'profile', label: 'Profile' },
+            { key: 'logout', label: 'Logout', icon: <LogoutOutlined />, danger: true },
+        ],
+        onClick: ({ key }: { key: string }) => {
+            if (key === 'logout') handleLogout();
+            if (key === 'profile') navigate('/profile');
+        },
+    };
+
+    const guestMenu = {
+        items: [
+            { key: 'login', label: 'LogIn' },
+        ],
+        onClick: ({ key }: { key: string }) => {
+            if (key === 'login') navigate('/login');
+        },
+    };
+
     return (
         <Layout>
-            <Header style={{ display: 'flex', alignItems: 'center' }}>
-                <div className="demo-logo" />
+            <Header style={{
+                display: 'flex',
+                alignItems: 'center',
+                justifyContent: 'space-between',
+                padding: '0 24px',
+            }}>
                 <Menu
                     theme="dark"
                     mode="horizontal"
@@ -28,6 +62,17 @@ const MainLayout = ({ children }: any) => {
                     style={{ flex: 1, minWidth: 0 }}
                     onClick={(e) => navigate(e.key)}
                 />
+
+                <Dropdown menu={user ? userMenu : guestMenu} placement="bottomRight" arrow>
+                    <Space style={{ cursor: 'pointer', color: 'white' }}>
+                        <Avatar
+                            src={user?.avatar}
+                            icon={!user?.avatar && <UserOutlined />}
+                            size="large"
+                        />
+                        <span>{user?.username ?? 'Guest'}</span>
+                    </Space>
+                </Dropdown>
             </Header>
 
             <Content style={{ padding: '0 48px' }}>
--- web/src/pages/LoginPage/index.tsx
+++ web/src/pages/LoginPage/index.tsx
@@ -1,51 +1,162 @@
-import type { FormProps } from 'antd';
-import { Button, Form, Input } from 'antd';
+import { Button, Form, Input, message, Tabs } from 'antd';
+import { useLocation, useNavigate } from 'react-router-dom';
+import { useAuth } from '../../contexts/AuthContext';
+import { login, register, type LoginPayload, type RegisterPayload } from '../../api/user';
+import { useState } from 'react'
 
 type FieldType = {
-    email?: string;
-    password?: string;
+    email: string;
+    password: string;
 };
 
-const onFinish: FormProps<FieldType>['onFinish'] = (values) => {
-    console.log('Success:', values);
-};
-
-const onFinishFailed: FormProps<FieldType>['onFinishFailed'] = (errorInfo) => {
-    console.log('Failed:', errorInfo);
+type RegisterFormFieldType = {
+    username: string;
+    email: string;
+    password: string;
 };
 
 export default function LoginPage() {
+    const navigate = useNavigate();
+    const location = useLocation();
+    const { setStatus } = useAuth();
+    const from = (location.state as any)?.from?.pathname || '/trips';
+
+    const [loadingLogin, setLoadingLogin] = useState(false);
+    const [loadingReg, setLoadingReg] = useState(false);
+
+    function onLogin(values: FieldType) {
+        setLoadingLogin(true);
+        const loginPayload: LoginPayload = {
+            email: values.email,
+            password: values.password,
+        }
+        login(loginPayload)
+            .then((token) => {
+                setStatus('authenticated');
+                localStorage.setItem('token', token);
+                navigate(from, { replace: true });
+            })
+            .catch((err: Error) => {
+                console.log(err)
+                alert(err.message) // temp
+                message.error(err.message || 'Login failed.'); //todo
+            })
+            .finally(() => {
+                setLoadingLogin(false);
+            })
+    }
+
+
+    function onRegister(values: RegisterFormFieldType) {
+        setLoadingReg(true);
+        const registerPayload: RegisterPayload = {
+            username: values.username,
+            email: values.email,
+            password: values.password,
+        }
+
+        register(registerPayload)
+            .then(() => {
+                login(registerPayload)
+                    .then((token) => {
+                        setStatus('authenticated');
+                        localStorage.setItem('token', token);
+                        navigate(from, { replace: true });
+                    })
+            })
+            .catch((err: any) => {
+                alert(err.message) // temp
+                message.error(err.message || 'Register failed.');//todo
+            })
+            .finally(() => {
+                setLoadingReg(false);
+            })
+    }
+
     return (
-        <Form
-            name="basic"
-            labelCol={{ span: 8 }}
-            wrapperCol={{ span: 16 }}
-            style={{ maxWidth: 600 }}
-            onFinish={onFinish}
-            onFinishFailed={onFinishFailed}
-            autoComplete="off"
-        >
-            <Form.Item<FieldType>
-                label="Email"
-                name="email"
-                rules={[{ type: 'email', required: true, message: 'Please input your email!' }]}
-            >
-                <Input />
-            </Form.Item>
+        <Tabs
+            defaultActiveKey="login"
+            items={[
+                {
+                    key: 'login',
+                    label: 'Login',
+                    children: (
+                        <Form
+                            name="login"
+                            labelCol={{ span: 8 }}
+                            wrapperCol={{ span: 16 }}
+                            style={{ maxWidth: 600 }}
+                            onFinish={onLogin}
+                            autoComplete="off"
+                        >
+                            <Form.Item<FieldType>
+                                label="Email"
+                                name="email"
+                                rules={[{ type: 'email', required: true, message: 'Please input your email!' }]}
+                            >
+                                <Input />
+                            </Form.Item>
 
-            <Form.Item<FieldType>
-                label="Password"
-                name="password"
-                rules={[{ required: true, message: 'Please input your password!' }]}
-            >
-                <Input.Password />
-            </Form.Item>
+                            <Form.Item<FieldType>
+                                label="Password"
+                                name="password"
+                                rules={[{ required: true, message: 'Please input your password!' }]}
+                            >
+                                <Input.Password />
+                            </Form.Item>
 
-            <Form.Item label={null}>
-                <Button type="primary" htmlType="submit">
-                    Log In Now
-                </Button>
-            </Form.Item>
-        </Form>
+                            <Form.Item label={null}>
+                                <Button type="primary" htmlType="submit" loading={loadingLogin}>
+                                    Sign in
+                                </Button>
+                            </Form.Item>
+                        </Form>
+                    )
+                },
+                {
+                    key: 'register',
+                    label: 'Register',
+                    children: (
+                        <Form
+                            name="register"
+                            labelCol={{ span: 8 }}
+                            wrapperCol={{ span: 16 }}
+                            style={{ maxWidth: 600 }}
+                            onFinish={onRegister}
+                            autoComplete="off"
+                        >
+                            <Form.Item<RegisterFormFieldType>
+                                label="Name"
+                                name="username"
+                                rules={[{ required: true, message: 'Please enter your name' }]}
+                            >
+                                <Input />
+                            </Form.Item>
+                            <Form.Item<RegisterFormFieldType>
+                                label="Email"
+                                name="email"
+                                rules={[{ type: 'email', required: true, message: 'Please input your email!' }]}
+                            >
+                                <Input />
+                            </Form.Item>
+
+                            <Form.Item<RegisterFormFieldType>
+                                label="Password"
+                                name="password"
+                                rules={[{ required: true, message: 'Please input your password!' }]}
+                            >
+                                <Input.Password />
+                            </Form.Item>
+
+                            <Form.Item label={null}>
+                                <Button type="primary" htmlType="submit" loading={loadingReg}>
+                                    Create account
+                                </Button>
+                            </Form.Item>
+                        </Form>
+                    )
+                }
+            ]}
+        />
     )
 }
