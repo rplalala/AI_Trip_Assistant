0a2df6607534c0bccdca8f3506ff0f371bbf02d1
caorong001 <caorong001@ke.com>
2025-10-22 10:33:21 +1100

    Home page desc

--- web/src/pages/HomePage/index.tsx
+++ web/src/pages/HomePage/index.tsx
@@ -17,6 +17,8 @@ export default function HomePage() {
 
     return (
         <div>
+            <div>Hello, this is home page</div>
+            <div>Hello again, this is home page</div>
             <Button onClick={handleStart}>Start</Button>
         </div>
     )
