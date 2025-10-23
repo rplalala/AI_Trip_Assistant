6df3d3111a740d9989292aa438d7200aedc5dc37
Zihang Ding <dingzh00@foxmail.com>
2025-10-23 19:29:06 +1100

    fix: compatible problem between antd v5 and react 19.

--- web/src/main.tsx
+++ web/src/main.tsx
@@ -2,6 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { App as AntdApp } from 'antd';
 import './index.css'
+import '@ant-design/v5-patch-for-react-19';
 import 'antd/dist/reset.css';
 import App from './App.tsx'
 
