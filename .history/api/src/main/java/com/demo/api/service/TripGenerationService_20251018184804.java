3e6750e9a226640840712dac8e7bbff5e2062291
Riley Lai <rileylai1101@gmail.com>
2025-10-28 14:58:30 +1100

    Remove .history folder from Git tracking and ignore it in future

--- .history/api/src/main/java/com/demo/api/service/TripGenerationService_20251018184804.java
+++
@@ -1,9 +0,0 @@
-package com.demo.api.service;
-
-import com.demo.api.dto.TripPreferenceRequestDTO;
-
-public interface TripGenerationService {
-
-    void generateTripFromPreference(TripPreferenceRequestDTO dto);
-}
-
